e7ce79fdb9d96c838fa9114d37633a053c7680d6
Rashad <rashad.walcott15@gmail.com>
2020-01-05 18:18:02 -0500

    Added line through for todoitems

--- src/App.js
+++ src/App.js
@@ -22,14 +22,26 @@ class App extends React.Component {
 },
     ]
   }
+  //Toggle Completed
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
+  }
   render() {
     // console.log(this.state.todos)
   return (
     <div className="App">
-      <Todos todos = {this.state.todos}/>
+      <Todos todos = {this.state.todos} markComplete={this.markComplete}/>
     </div>
     )
   }
 }
+
 
 export default App;
