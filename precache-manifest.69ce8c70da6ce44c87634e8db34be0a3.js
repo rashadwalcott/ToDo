ad2a2de58aa0eb776f3d0e74abfd9dc1ce605923
Rashad <rashad.walcott15@gmail.com>
2020-01-05 20:32:10 -0500

    Updates

---
+++ precache-manifest.69ce8c70da6ce44c87634e8db34be0a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eaeabedb16f937cbb9f3baaed98641b",
+    "url": "/ToDo/home/index.html"
+  },
+  {
+    "revision": "c4f653de8714a7696f71",
+    "url": "/ToDo/home/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "b8d4db5880317bec6592",
+    "url": "/ToDo/home/static/js/2.7b482595.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/ToDo/home/static/js/2.7b482595.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4f653de8714a7696f71",
+    "url": "/ToDo/home/static/js/main.cabc4046.chunk.js"
+  },
+  {
+    "revision": "0fa940d0828ab6444446",
+    "url": "/ToDo/home/static/js/runtime-main.fa99cec5.js"
+  }
+]);
